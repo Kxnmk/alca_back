96d5c7094c87737eafe19192de4d81d1d3b34ac2
Gabriel <gabriellopezdv@gmail.com>
2017-11-14 15:51:19 -0600

    hola response

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var usuarios = require('./routes/usuarios');
+
 var app = express();
 
 // view engine setup
@@ -24,6 +26,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//rutas
+app.use('/api/usuarios',usuarios);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/usuario.js
@@ -0,0 +1,29 @@
+//var db=require('../dbconnection');
+
+var usuario={
+
+  // getAllUsuarios:function(callback){
+  //   return db.query("Select * from Usuarios",callback);
+  // },
+  //
+  // getUsuarioById:function(id,callback){
+  //
+  //     return db.query("select * from Usuarios where usrClave=?",[id],callback);
+  // },
+  //
+  // addUsuario:function(u,callback){
+  //
+  //    return db.query("Insert into Usuarios values(?,?,?,?,?,?)",[u.usrClave,u.usrNombre,u.usrRol,u.usrName,u.usrPassword,u.usrRandom],callback);
+  //
+  // },
+  // deleteUsuario:function(id,callback){
+  //     return db.query("delete from Usuarios where usrClave=?",[id],callback);
+  // },
+  // updateUsuario:function(id,u,callback){
+  //     return  db.query("update Usuarios set usrNombre=?,usrRol=?,usrName=?,usrPassword=?,usrRandom=? where usrClave=?",[u.usrNombre,u.usrRol,u.usrName,u.usrPassword,u.usrRandom,u.usrClave],callback);
+  // },
+  // searchUsuario:function(d,callback){
+  //     return  db.query("select * from Usuarios where usrName=? and usrPassword=?",[d.usrName,d.usrPassword],callback);
+  // }
+}
+module.exports=usuario;
